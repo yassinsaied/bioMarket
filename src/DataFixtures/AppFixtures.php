39cabe336a515ce10e949e35c5d440c914863ded
yassinsaied <saied.yassin@gmail.com>
2020-11-02 01:42:50 +0100

    first commit

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+
+
+class AppFixtures extends Fixture
+{
+
+
+    private static $unite = [
+
+        "piece",
+        "Kg",
+        "100g"
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+
+        $faker = Factory::create("en_US");
+        $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+
+        $category = new Category;
+        $category->setName("fruits");
+        $manager->persist($category);
+
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Product;
+            $product->setName($faker->unique()->fruitName);
+            $product->setDescription($faker->realText(50));
+            $product->setPrice($faker->randomFloat(2, 1, 10));
+            $product->setUnite($faker->randomElement(self::$unite));
+            $product->setPromo($faker->numberBetween(10, 40));
+            $product->setCategory($category);
+            $manager->persist($product);
+        }
+
+        $category2 = new Category;
+        $category2->setName("vegetables");
+        $manager->persist($category2);
+
+        for ($i = 0; $i < 20; $i++) {
+            $product2 = new Product;
+            $product2->setName($faker->unique()->vegetableName);
+            $product2->setDescription($faker->realText(50));
+            $product2->setPrice($faker->randomFloat(2, 1, 10));
+            $product2->setUnite($faker->randomElement(self::$unite));
+            $product2->setPromo($faker->numberBetween(10, 40));
+            $product2->setCategory($category2);
+            $manager->persist($product2);
+        }
+
+        $category3 = new Category;
+        $category3->setName("meats");
+        $manager->persist($category3);
+
+        for ($i = 0; $i < 30; $i++) {
+            $product3 = new Product;
+            $product3->setName($faker->meatName());
+            $product3->setDescription($faker->realText(50));
+            $product3->setPrice($faker->randomFloat(2, 1, 10));
+            $product3->setUnite($faker->randomElement(self::$unite));
+            $product3->setPromo($faker->numberBetween(10, 40));
+            $product3->setCategory($category3);
+            $manager->persist($product3);
+        }
+
+
+
+
+
+
+
+
+
+        // $product = new Product();
+        // $manager->persist($product);
+
+
+        $manager->flush();
+    }
+}
